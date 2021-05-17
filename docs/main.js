cb63ae8872c66cbe53e5f9c3d39ca7bb0caf3e2f
Benedikt Dornauer <2010738480@stud.fh-kufstein.ac.at>
2021-05-17 13:36:28 +0200

    For deploy

--- docs/main.js
+++ docs/main.js
@@ -140,13 +140,13 @@ function distanceInMeters(lat1, lon1, lat2, lon2) {
 
     var dLat = degreesToRadians(lat2 - lat1);
     var dLon = degreesToRadians(lon2 - lon1);
-  
+
     lat1 = degreesToRadians(lat1);
     lat2 = degreesToRadians(lat2);
-  
+
     var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2); 
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
     return earthRadius * c;
 }
